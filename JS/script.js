6aa1dd17992e2eafb5977249461de77f95684dbb
Kiki <kabdollahi347@gmail.com>
2023-03-16 17:02:50 -0700

    Adds script tag to course.html and creates const array of courses

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+
+const courseList = [{code: "ACIT 1420", 
+                     name:"Intro to System Administration"
+                        },
+
+                    {code: "MATH 1310",
+                     name: "Technical Math for IT"   
+                        }, 
+
+                    {code:"ACIT 1630",
+                     name:"Database Systems"
+
+                        }
+                    ]
